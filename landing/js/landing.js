9cafba3c93d035c715c5983d3ef5d16955e8dc7f
이동춘 <ldc84@naver.com>
2022-06-29 00:14:38 +0900

    깃허브 페이지에 반영됨. Published to Github pages

--- landing/js/landing.js
+++ landing/js/landing.js
@@ -1,1 +1,1 @@
-AOS.init(),$(function(){$(".landing-client > .client").find("> div").each(function(n){var i=$(this),t=i.html();i.append(t)})});
+AOS.init(),$(function(){$(".landing-client > .client").find("> div").each(function(i){var n=$(this),t=n.html();n.append(t)}),$(".people-box.m-only").slick({dots:!0,arrows:!1,infinite:!1})});
